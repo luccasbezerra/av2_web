c19af57e4b0182660ba3020dc2adf1f082184b1a
luccasbezerra <45153434+luccasbezerra@users.noreply.github.com>
2021-03-04 22:59:18 -0300

    Method Create and Delete

--- controllers/livro.controller.js
+++ controllers/livro.controller.js
@@ -1,9 +1,35 @@
 var Livro = require('../models/livro.model');
 
+exports.create = function (req, res) {
+    console.log(req.body)
+    let livro = new Livro({
+        nomeLivro: req.body.nomeLivro,
+        autor: req.body.autor,
+        editora: req.body.editora,
+        dataLancamento: Date.parse(req.body.dataLancamento),
+        volume: parseInt(req.body.volume),
+        isbn: parseInt(req.body.isbn)});
+
+    livro.save().then((data)=> {
+        console.log(data);
+        res.send('Registo de Livro criado com sucesso')
+    }).catch((err)=> {
+        console.log(err);
+    })
+};
+
 exports.read = function (req, res) {
     Livro.findOne({isbn: parseInt(req.params.isbn)}, function (err, livro) {
         if (err) return err;
             res.send(livro);
+    })  
+};
+
+exports.delete = function (req, res) {
+    console.log(req.params)
+    Livro.findOneAndRemove({isbn: parseInt(req.params.isbn)}, function (err) {
+        if (err) return err;
+        else res.send('Livro Deletado')
     })
 };
 
--- routes/livro.route.js
+++ routes/livro.route.js
@@ -3,6 +3,10 @@ const router = express.Router();
 // Colocar controller que ainda não foi criado
 const livro_controller = require('../controllers/livro.controller');
 
+router.post('/create', livro_controller.create);
+
 router.get('/:isbn', livro_controller.read);
+
+router.delete('/delete/:isbn', livro_controller.delete);
 
 module.exports = router;
